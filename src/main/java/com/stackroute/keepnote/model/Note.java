e30c7c495c60aa2fa5a25ac85827279048276688
Ruchi <ruchi.kumari4@cognizant.com>
2018-10-28 10:08:56 +0000

    Coding

---
+++ src/main/java/com/stackroute/keepnote/model/Note.java
@@ -0,0 +1,92 @@
+package com.stackroute.keepnote.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/*
+ * The class "Note" will be acting as the data model for the note Table in the database. Please
+ * note that this class is annotated with @Entity annotation. Hibernate will scan all package for 
+ * any Java objects annotated with the @Entity annotation. If it finds any, then it will begin the 
+ * process of looking through that particular Java object to recreate it as a table in your database.
+ */
+@Entity(name = "Note")
+public class Note {
+
+  @Id
+  @Column(name="note_id")
+  @GeneratedValue(strategy=GenerationType.IDENTITY)
+  private int noteId;
+  
+  @Column(name ="note_title")
+  private String noteTitle;
+  
+  @Column(name ="note_content")
+  private String noteContent;
+  
+  @Column(name ="note_status")
+  private String noteStatus;
+  
+  @Column(name ="note_creation_date")
+  private LocalDateTime createdAt;
+  
+  
+	public Note() {
+
+
+	}
+
+	public Note(int noteId, String noteTitle, String noteContent, String noteStatus, LocalDateTime createdAt) {
+		super();
+		this.noteId = noteId;
+		this.noteTitle = noteTitle;
+		this.noteContent = noteContent;
+		this.noteStatus = noteStatus;
+	    this.createdAt = createdAt;
+	}
+
+	public int getNoteId() {
+
+		return noteId;
+	}
+
+	public String getNoteTitle() {
+
+		return noteTitle;
+	}
+
+	public String getNoteContent() {
+
+		return noteContent;
+	}
+
+	public String getNoteStatus() {
+
+		return noteStatus;
+	}
+
+	public void setNoteId(int noteId) {
+    this.noteId = noteId;
+	}
+
+	public void setNoteTitle(String noteTitle) {
+     this.noteTitle = noteTitle;
+	}
+
+	public void setNoteContent(String noteContent) {
+     this.noteContent = noteContent;
+	}
+
+	public void setNoteStatus(String noteStatus) {
+      this.noteStatus = noteStatus;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+      this.createdAt = createdAt;
+	}
+
+}
